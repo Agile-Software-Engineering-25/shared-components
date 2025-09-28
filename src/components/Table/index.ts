f4cd9308812c082827cb58655b0de612505d3d66
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-09-28 21:46:54 +0200

    change table example naming

--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -1,28 +1,27 @@
 // Main Table component
-export { default as Table } from './components/Table';
+export { default as Table } from "./components/Table";
 
 // Individual components
-export { default as DesktopTable } from './components/DesktopTable';
-export { default as MobileTable } from './components/MobileTable';
-export { default as TableHeader } from './components/TableHeader';
-export { default as TableRow } from './components/TableRow';
-export { default as TableCell } from './components/TableCell';
-export { default as Pagination } from './components/Pagination';
-export { default as FilterBar } from './components/FilterBar';
-export { default as RowActions } from './components/RowActions';
-export { default as EmptyState } from './components/EmptyState';
-export { default as LoadingState } from './components/LoadingState';
+export { default as DesktopTable } from "./components/DesktopTable";
+export { default as MobileTable } from "./components/MobileTable";
+export { default as TableHeader } from "./components/TableHeader";
+export { default as TableRow } from "./components/TableRow";
+export { default as TableCell } from "./components/TableCell";
+export { default as Pagination } from "./components/Pagination";
+export { default as FilterBar } from "./components/FilterBar";
+export { default as RowActions } from "./components/RowActions";
+export { default as EmptyState } from "./components/EmptyState";
+export { default as LoadingState } from "./components/LoadingState";
 
 // Hooks
-export * from './hooks';
+export * from "./hooks";
 
 // Types
-export * from './types';
+export * from "./types";
 
 // Utilities
-export * from './utils';
-
+export * from "./utils";
 
 // TODO: remove DEBUG
-export { default as AdvancedExample } from './examples/AdvancedExample';
-export { default as BasicExample } from './examples/BasicExample';
+export { default as AdvancedExample } from "./examples/AdvancedExample";
+export { default as BasicExample } from "./examples/BasicExample";
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 // Export components
-export { default as Modal } from './components/Modal/Modal';
-export { Table } from './components/Table';
+export { default as Modal } from "./components/Modal/Modal";
+export { Table } from "./components/Table";
 // TODO: DEBUG remove this two imports
-export { default as AdvancedExample } from './components/Table/examples/AdvancedExample';
-export { default as BasicExample } from './components/Table/examples/BasicExample';
+export { default as TableExampleAdvanced } from "./components/Table/examples/AdvancedExample";
+export { default as TableExampleBasic } from "./components/Table/examples/BasicExample";
 
 // Export types
-export type { ModalProps } from './components/Modal/Modal';
-export * from './components/Table/types';
+export type { ModalProps } from "./components/Modal/Modal";
+export * from "./components/Table/types";
 
 // Export theme utilities
-export { createCustomJoyTheme, createCustomMuiTheme } from './theme/theme';
+export { createCustomJoyTheme, createCustomMuiTheme } from "./theme/theme";
 
 // Export table utilities
-export * from './components/Table/utils';
-export * from './components/Table/hooks';
+export * from "./components/Table/utils";
+export * from "./components/Table/hooks";
